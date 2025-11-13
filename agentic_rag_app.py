e4b3e730cea9e2fbee2418afa7e074522210dc68
Dhiraj Kumar <gdhiraj499@gmail.com>
2025-11-13 12:37:23 +0530

    Setup API keys

--- agentic_rag_app.py
+++ agentic_rag_app.py
@@ -6,4 +6,9 @@ from crewai_tools import PDFSearchTool
 from langchain_community.tools.tavily_search import TavilySearchResults
 from crewai_tools import tools
 from crewai import Crew, Task, Agent
+from dotenv import load_dotenv
 
+load_dotenv()
+
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+TAVILY_API_KEY=os.getenv("TAVILY_API_KEY")
